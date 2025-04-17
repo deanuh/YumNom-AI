14b88a7320b8966940e31f192021e4666df1a7e5
Danniella M <167385272+danniemtz@users.noreply.github.com>
2025-04-16 19:49:31 -0700

    worked on the settings page and test. need to finish making all the pages for settings to redirect to. settings.css in styles folder

---
+++ frontend/src/pages/settings.js
@@ -0,0 +1,48 @@
+// // this will be the display for the settings page
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/settings.css";
+
+function Settings() {
+  return (
+    <div className="settings-wrapper">
+      <div className="settings-card">
+        <h2 className="settings-title">Settings & Activity</h2>
+
+        <div className="settings-section">
+          <h3 className="section-heading">General Settings</h3>
+          <ul className="settings-list">
+            <li><Link to="#">Change password</Link></li>
+            <li><Link to="#">Email</Link></li>
+            <li><Link to="#">Location Preferences</Link></li>
+            <li><Link to="#">Notifications</Link></li>
+            <li><Link to="#">Display</Link></li>
+          </ul>
+        </div>
+
+        <div className="settings-section">
+          <h3 className="section-heading">Legal</h3>
+          <ul className="settings-list">
+            <li><Link to="/terms">Terms</Link></li>
+            <li><Link to="/privacy-policy">Privacy Policy</Link></li>
+          </ul>
+        </div>
+
+        <div className="settings-section">
+          <h3 className="section-heading">Account</h3>
+          <ul className="settings-list">
+            <li><Link to="/help">Help</Link></li>
+            <li><Link to="#">Report Abuse</Link></li>
+            <li><Link to="#">Delete Account</Link></li>
+            <li><Link to="#">Log out</Link></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Settings;
+
+
+
---
+++ frontend/src/settingsTest.js
@@ -0,0 +1,31 @@
+//this will be the test for running settings (basically app.js)
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// Pages
+import Settings from "./pages/settings";
+import Terms from "./pages/settingsInfo/terms";
+import ChangePassword from "./pages/settingsInfo/changePass";
+import DeleteAccount from "./pages/settingsInfo/deleteAccount";
+import EmailSettings from "./pages/settingsInfo/emailSettings";
+import Help from "./pages/settingsInfo/help";
+import PrivacyPolicy from "./pages/settingsInfo/privacyPolicy";
+
+
+function SettingsTest() {
+  return (
+    // the /settings/* tells the app that the settings.js file will handle the routes to the other pages
+    // such as the terms.js file in settingsInfo/terms.js
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/settings" replace />} />
+        <Route path="/settings/*" element={<Settings />} /> 
+        <Route path="/terms" element={<Terms />} />  
+         
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default SettingsTest;
+
