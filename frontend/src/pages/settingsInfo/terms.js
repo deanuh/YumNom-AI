414a439dc585341751f58f764f7ce6b03a0bc961
Danniella M <167385272+danniemtz@users.noreply.github.com>
2025-04-16 19:49:31 -0700

    worked on the settings page and test. need to finish making all the pages for settings to redirect to. settings.css in styles folder

--- frontend/src/pages/settingsInfo/terms.js
+++ frontend/src/pages/settingsInfo/terms.js
@@ -1,2 +1,12 @@
 // what entails having an account with YumNom 
 // such as having location on and personal data such as preferences made available to us
+// Terms.js
+import "../../styles/settings.css";
+function Terms() {
+    return <div className="settings-card"><h3>Terms & Conditions</h3>
+    <p>All about terms...</p></div>;
+  }
+  export default Terms;
+  
+
+  
