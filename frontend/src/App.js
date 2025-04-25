7ba12919f162892274e89cdc49588ae068dc9e2f
Danniella M <167385272+danniemtz@users.noreply.github.com>
2025-04-24 23:26:07 -0700

    fixed up the app.js that should be able to now connect to the other pages we have finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,16 @@ import RestaurantSearch from "./pages/restaurantSearch";
 import Settings from "./pages/settings";
 import UserProfilePage from "./pages/UserProfilePage";
 
+// Settings Pages
+import Settings from "./pages/settings";
+import Terms from "./pages/settingsInfo/terms";
+import ChangePassword from "./pages/settingsInfo/changePass";
+import DeleteAccount from "./pages/settingsInfo/deleteAccount";
+import EmailSettings from "./pages/settingsInfo/emailSettings";
+import Help from "./pages/settingsInfo/help";
+import PrivacyPolicy from "./pages/settingsInfo/privacyPolicy";
+import LocationPref from "./pages/settingsInfo/locationPref";
+
 function App() {
   return (
     <Router>
@@ -18,6 +28,13 @@ function App() {
             <Route path="/restaurantSearch" element={<RestaurantSearch />} />
             <Route path="/settings" element={<Settings />} /> 
             <Route path="/userprofile" element={<UserProfilePage />} />
+            <Route path="/terms" element={<Terms />} />  
+            <Route path="/changePass" element={<ChangePassword />} />
+            <Route path="/emailSettings" element={<EmailSettings />} /> 
+            <Route path="/locationPref" element={<LocationPref />} />
+            <Route path="/deleteAccount" element={<DeleteAccount />} /> 
+            <Route path="/help" element={<Help />} /> 
+            <Route path="/privacyPolicy" element={<PrivacyPolicy />} /> 
           </Routes>
         </div>
       </div>
