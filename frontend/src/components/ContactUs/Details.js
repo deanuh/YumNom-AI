35afd39f1be891bd7f86de5163dc8696fe210179
Martin Silva <Martin.Silva01@student.csulb.edu>
2025-04-25 20:04:48 -0700

    prototype Contact Us page, changes to index for fonts.

---
+++ frontend/src/components/ContactUs/Details.js
@@ -0,0 +1,14 @@
+const Details = () => {
+    return (
+        <div className="detailsColumn">
+            <h1>Contact Us</h1>
+            <h3>We are here to help!</h3>
+            <p>Have some questions or concerns? Let us know what you need help with! Leave us a comment and don't be afraid to reach out.</p>
+            
+            <h2>Email</h2>
+            <p>yumnomai@gmail.com</p>
+
+        </div>
+    );
+}
+export default Details;
