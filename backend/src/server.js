266a27790cd38bb635832bcf1671f67f0ece2675
Martin Silva <Martin.Silva01@student.csulb.edu>
2025-08-26 13:58:28 -0700

    changed port to 5001 instead of 5000 due to airplay port on mac

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ app.get('/restaurant', getRestaurantTripAdvisor);
 app.get('/food', getFoodFatSecret);
 app.get('/city', getUserCityOpenCage);
 
-app.listen(5000, () => {
-console.log('listening on port 5000')});
+app.listen(5001, () => {
+console.log('listening on port 5001')});
 
 
