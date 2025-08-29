e4730ae8be51ef617b0f96d9aa6f2c79621541e9
Martin Silva <Martin.Silva01@student.csulb.edu>
2025-08-28 17:33:43 -0700

    connected seed script to development emulator

--- frontend/runSeed.js
+++ frontend/runSeed.js
@@ -1,4 +1,8 @@
 import 'dotenv/config';
+import { db, auth } from "./src/firebase/firebaseConfig.js";
+import { connectFirestoreEmulator } from "firebase/firestore";
+import { connectAuthEmulator } from "firebase/auth";
+
 import { seed } from "./src/firebase/seed.js";
 
 async function runSeed() {
@@ -6,6 +10,10 @@ async function runSeed() {
     await seed();
     console.log("Seeding complete!");
 }
+
+//for development
+connectFirestoreEmulator(db, "localhost", 8080);
+connectAuthEmulator(auth, "http://localhost:9099");
 
 runSeed().catch(console.error);
 
